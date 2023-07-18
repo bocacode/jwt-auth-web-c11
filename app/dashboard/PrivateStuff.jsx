846acb07a92aea85065142786d7cd03ba98e15ed
Todd H. Albert <todd@bocacode.com>
2023-07-18 12:35:35 -0400

    creating truly private data

---
+++ app/dashboard/PrivateStuff.jsx
@@ -0,0 +1,5 @@
+export default function PrivateStuff() {
+  return (
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, eum? Hic soluta et, nulla nesciunt cumque nam minus eos rem sequi ea dicta aut. Aliquid assumenda nemo dolores esse perferendis!</p>
+  )
+}
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,10 +1,18 @@
+import Link from "next/link"
 import CheckAuth from "./CheckAuth"
+import PrivateStuff from "./PrivateStuff"
+import Profile from "./Profile"
 
 export default function Dashboard() {
   return (
-    <main>
-      <h1>Dashboard</h1>
+    <main className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
       <CheckAuth />
+      <h2 className="text-lg mb-4 font-semibold text-blue-200">Private (sorta) stuff</h2>
+      <PrivateStuff />
+      <h2 className="text-lg mb-4 font-semibold text-blue-200">Profle (truly private):</h2>
+      <Profile />
+      <Link href="/" className="text-indigo-400 pt-8">Home</Link>
     </main>
   )
 }
