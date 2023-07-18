d36805d49ffc63e3e3f3fc75f3a57d014c457bb7
Todd H. Albert <todd@bocacode.com>
2023-07-18 10:54:59 -0400

    wrap app in context; update metadata

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -1,11 +1,21 @@
 "use client"
-import { createContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
 
 export const AuthContext = createContext()
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState()
   const [token, setToken] = useState()
+
+  useEffect(() => {
+    // on load or refresh, check if user was previously logged-in
+    if(!user) {
+      const previousUser = sessionStorage.getItem("user")
+      const previousToken = sessionStorage.getItem("token")
+      if(previousUser) setUser(JSON.parse(previousUser))
+      if(previousToken) setToken(JSON.parse(previousToken))
+    }
+  }, [])
 
   const handleLogin = (data) => {
     setUser(data.user)
